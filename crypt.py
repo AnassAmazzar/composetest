0c5d9d41788959b908e8aa6876c5014648ab16a0
Administrator <gitlab_admin_cc90b0@example.com>
2025-03-19 13:42:14 +0000

    Commit in my web app

---
+++ crypt.py
@@ -0,0 +1,20 @@
+# import bcrypt
+
+# password="anass123"
+# hashed = bcrypt.hashpw(password.encode("utf-16"), bcrypt.gensalt())
+
+# print(hashed)
+
+from hashlib import pbkdf2_hmac
+
+salt = b"mon_slat"
+stored_hash = b"votre_hachage_pbkdf2"
+iterations = 100000
+
+
+
+
+password="anass123"
+hashed = pbkdf2_hmac('sha256', password.encode("utf-8"), salt, iterations)
+
+print(hashed)
