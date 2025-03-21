e14e5c33ef91e6db6ea1641e79c758c41d0348df
Administrator <gitlab_admin_cc90b0@example.com>
2025-03-21 10:41:58 +0000

    Update file

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def hello():
     count = get_hit_count()
     return f'Hello World! I have been seen web site {count} times.\n I see you'
 
-@app.route('/home')
-def hello():
-    count = get_hit_count()
-    return f'Hello Anass Welcom to your website.'
+# @app.route('/home')
+# def hello():
+#     count = get_hit_count()
+#     return f'Hello Anass Welcom to your website.'
